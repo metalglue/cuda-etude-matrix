9a626af0d8e09170571d10b26fbeda89e30f02f1
Norio Kimura <kimura@fermat.local>
2011-09-04 21:17:46 +0900

    stopwatch

--- aa.cpp
+++ aa.cpp
@@ -5,6 +5,10 @@
 
 typedef double number;
 
+/*        */
+/* matrix */
+/*        */
+
 class matrix {
 public:
     static matrix *new_random_filled(int height, int width);
@@ -40,7 +44,7 @@ public:
         number *pix;
         number *beyond;
     };
-    static void mul_ijk(const matrix *a, const matrix *b, matrix *r);
+    static int mul_ijk(const matrix *a, const matrix *b, matrix *r);
 private:
     matrix();
     matrix(int height_, int width_) : height(height_), width(width_) {}
@@ -88,7 +92,7 @@ void matrix::show() const
     printf(")\n");
 }
 
-void matrix::mul_ijk(const matrix *a, const matrix *b, matrix *r)
+int matrix::mul_ijk(const matrix *a, const matrix *b, matrix *r)
 {
     int width, height;
     a->size(&height, &width);
@@ -106,7 +110,52 @@ void matrix::mul_ijk(const matrix *a, const matrix *b, matrix *r)
             r_i++;
         }
     }
+    return width * width * height * 2;
 }
+
+/*           */
+/* stopwatch */
+/*           */
+
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/times.h>
+
+class stopwatch {
+public:
+    void start();
+    void get_lap();
+    void show(int flop_count) const ;
+private:
+    clock_t clock_start, clock_lap;
+    tms tms_start, tms_lap;
+};
+
+void stopwatch::start()
+{
+    clock_start = ::clock();
+    ::times(&tms_start);
+}
+
+void stopwatch::get_lap()
+{
+    clock_lap = ::clock();
+    ::times(&tms_lap);
+}
+
+void stopwatch::show(int flop_count) const
+{
+    long ticks = ::sysconf(_SC_CLK_TCK);
+    printf("Real: %.2f s\n", (double)(clock_lap - clock_start) / CLOCKS_PER_SEC);
+    printf("User: %.2f s\n", (double)(tms_lap.tms_utime - tms_start.tms_utime) / ticks);
+    printf(" Sys: %.2f s\n", (double)(tms_lap.tms_stime - tms_start.tms_stime) / ticks);
+    printf("%.2f GFLOPS\n", (double)flop_count / (clock_lap - clock_start) * CLOCKS_PER_SEC / 1000 / 1000 / 1000);
+}
+
+/*      */
+/* main */
+/*      */
 
 void
 test_0001()
@@ -114,8 +163,28 @@ test_0001()
     matrix *a = matrix::new_random_filled(2, 3);
     matrix *b = matrix::new_random_filled(3, 2);
     matrix *r = matrix::new_garbage(2, 2);
-    matrix::mul_ijk(a, b, r);
+    stopwatch sw;
+    sw.start();
+    int flop_count = matrix::mul_ijk(a, b, r);
+    sw.get_lap();
     a->show(); b->show(); r->show();
+    sw.show(flop_count);
+    matrix::delete_matrix(a);
+    matrix::delete_matrix(b);
+    matrix::delete_matrix(r);
+}
+
+void
+test_0002()
+{
+    matrix *a = matrix::new_random_filled(1000, 1000);
+    matrix *b = matrix::new_random_filled(1000, 1000);
+    matrix *r = matrix::new_garbage(1000, 1000);
+    stopwatch sw;
+    sw.start();
+    int flop_count = matrix::mul_ijk(a, b, r);
+    sw.get_lap();
+    sw.show(flop_count);
     matrix::delete_matrix(a);
     matrix::delete_matrix(b);
     matrix::delete_matrix(r);
@@ -123,6 +192,6 @@ test_0001()
 
 int main()
 {
-    test_0001();
+    test_0002();
 }
 
