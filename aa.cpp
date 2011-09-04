2926fcbdd4ec44005d42bf2bdf85eb1849375a33
Norio Kimura <kimura@fermat.local>
2011-09-04 22:59:22 +0900

    mul_kij()

--- aa.cpp
+++ aa.cpp
@@ -2,6 +2,7 @@
 
 #include <cstdlib>
 #include <stdio.h>
+#include <assert.h>
 
 const int DIM = 1000;
 typedef double number;
@@ -20,6 +21,7 @@ public:
         iter_all(const matrix *m) { pix = &m->items[0]; beyond = &m->items[m->height * m->width]; }
         void operator ++(int) { pix++; }
         number *operator *() { return pix == beyond ? 0 : pix; }
+        void ok() { assert(pix <= beyond); }
     private:
         number *pix;
         number *beyond;
@@ -31,6 +33,7 @@ public:
         iter_row(const matrix *m, int row) { pix = &m->items[row * m->width]; beyond = &m->items[(row + 1) * m->width]; }
         void operator ++(int) { pix++; }
         number *operator *() { return pix == beyond ? 0 : pix; }
+        void ok() { assert(pix <= beyond); }
     private:
         number *pix;
         number *beyond;
@@ -40,12 +43,15 @@ public:
         iter_col(const matrix *m, int col) : width(m->width) { pix = &m->items[col]; beyond = &m->items[m->height * m->width + col]; }
         void operator ++(int) { pix += width; }
         number *operator *() { return pix == beyond ? 0 : pix; }
+        void ok() { assert(pix <= beyond); }
     private:
         const int width;
         number *pix;
         number *beyond;
     };
+    static bool eq(const matrix *a, const matrix *b);
     static int mul_ijk(const matrix *a, const matrix *b, matrix *r);
+    static int mul_kij(const matrix *a, const matrix *b, matrix *r);
 private:
     matrix();
     matrix(int height_, int width_) : height(height_), width(width_) {}
@@ -93,6 +99,17 @@ void matrix::show() const
     printf(")\n");
 }
 
+bool matrix::eq(const matrix *a, const matrix *b)
+{
+    assert(a->height == b->height && a->width == b->width);
+    iter_all ia(a);
+    iter_all ib(b);
+    for (; *ia; ia++, ib++)
+        if (**ia != **ib)
+            return false;
+    return true;
+}
+
 int matrix::mul_ijk(const matrix *a, const matrix *b, matrix *r)
 {
     int width, height;
@@ -109,6 +126,29 @@ int matrix::mul_ijk(const matrix *a, const matrix *b, matrix *r)
             }
             **r_i = n;
             r_i++;
+        }
+    }
+    return width * width * height * 2;
+}
+
+int matrix::mul_kij(const matrix *a, const matrix *b, matrix *r)
+{
+    int width, height, size;
+    a->size(&height, &width);
+    for (iter_all i(r); *i; i++) {
+        **i = 0;
+        i.ok();
+    }
+    for (int k = 0; k < width; k++) {
+        iter_col a_i(a, k);
+        for (int i = 0; i < height; i++) {
+            double n = **a_i; a_i++;
+            iter_row b_i(b, k);
+            iter_row r_i(r, i);
+            for (int j = 0; j < height; j++) {
+                **r_i += **b_i * n;
+                r_i++; b_i++;
+            }
         }
     }
     return width * width * height * 2;
@@ -191,8 +231,65 @@ test_0002()
     matrix::delete_matrix(r);
 }
 
+void
+test_0003()
+{
+    matrix *a = matrix::new_random_filled(2, 3);
+    matrix *b = matrix::new_random_filled(3, 2);
+    matrix *r = matrix::new_garbage(2, 2);
+    stopwatch sw;
+    sw.start();
+    int flop_count = matrix::mul_kij(a, b, r);
+    sw.get_lap();
+    a->show(); b->show(); r->show();
+    sw.show(flop_count);
+    matrix::delete_matrix(a);
+    matrix::delete_matrix(b);
+    matrix::delete_matrix(r);
+}
+
+void
+test_0004()
+{
+    matrix *a = matrix::new_random_filled(DIM, DIM);
+    matrix *b = matrix::new_random_filled(DIM, DIM);
+    matrix *r = matrix::new_garbage(DIM, DIM);
+    stopwatch sw;
+    sw.start();
+    int flop_count = matrix::mul_kij(a, b, r);
+    sw.get_lap();
+    sw.show(flop_count);
+    matrix::delete_matrix(a);
+    matrix::delete_matrix(b);
+    matrix::delete_matrix(r);
+}
+
+void
+test_0005()
+{
+    matrix *a = matrix::new_random_filled(DIM, DIM);
+    matrix *b = matrix::new_random_filled(DIM, DIM);
+    matrix *r1 = matrix::new_garbage(DIM, DIM);
+    matrix *r2 = matrix::new_garbage(DIM, DIM);
+    stopwatch sw;
+    sw.start();
+    int flop_count;
+    flop_count = matrix::mul_ijk(a, b, r1);
+    sw.get_lap();
+    sw.show(flop_count);
+    sw.start();
+    flop_count = matrix::mul_kij(a, b, r2);
+    sw.get_lap();
+    sw.show(flop_count);
+    assert(matrix::eq(r1, r2));
+    matrix::delete_matrix(a);
+    matrix::delete_matrix(b);
+    matrix::delete_matrix(r1);
+    matrix::delete_matrix(r2);
+}
+
 int main()
 {
-    test_0002();
+    test_0005();
 }
 
